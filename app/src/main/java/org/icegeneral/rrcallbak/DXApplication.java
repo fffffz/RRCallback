b4a8e88abbe9098cb742db2df6a09f5403e1e674
jianjun.lin <jianjun.lin@baidao.com>
2016-07-21 10:44:11 +0800

    X

--- app/src/main/java/org/icegeneral/rrcallbak/DXApplication.java
+++ app/src/main/java/org/icegeneral/rrcallbak/DXApplication.java
@@ -5,7 +5,7 @@ import android.app.Application;
 import org.icegeneral.rrcallbak.retrofit.DX168API;
 
 /**
- * Created by jianjun.lin on 16/7/20.
+ * Created by iceGeneral on 16/7/20.
  */
 
 public class DXApplication extends Application {
